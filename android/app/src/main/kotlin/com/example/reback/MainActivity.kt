8f027fbd958b8799b2dbdfc40a5052a96e76e06b
Ahmed <farazali506@gmail.com>
2022-03-10 21:50:07 +0500

    intial commit

---
+++ android/app/src/main/kotlin/com/example/reback/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reback
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.black12,
+        leading: Icon(
+          Icons.games,
+        ),
+        title: Text(
+          'Reback',
+          style: TextStyle(
+            fontStyle: FontStyle.italic,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: basic(),
+    ),
+  ));
+}
+
+class basic extends StatelessWidget {
+  const basic({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('images/AppleAvatar.jpg'),
+              radius: 90.0,
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text(
+              'Ahmed Farz Ali',
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black54
+              ),
+            ),
+            Text(
+              'Passive Investor',
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 5.0,
+                color: Colors.black26,
+              ),
+            ),
+            SizedBox(
+              height: 5.0,
+              child: Divider(
+                thickness: 5.0,
+                indent: 50.0,
+                endIndent: 50.0,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 20.0,horizontal: 20.0),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                  ),
+                  title: Text('ahmedfarazali@yahoo.com'),
+                  trailing: Icon(
+                    Icons.add,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 2.0,horizontal: 20.0),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                  ),
+                  title: Text('03050237044'),
+                  trailing: Icon(
+                    Icons.add,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
